0127fb03a482f7adcfb3d4fe9c04d3aee3f652b5
Anqi Shao <anqi.shao@wisc.edu>
2024-04-23 14:33:34 -0500

    Update test-chat.py

--- test-chat.py
+++ test-chat.py
@@ -2,6 +2,16 @@ import streamlit as st
 import random
 import time
 
+from utils.session import session_setup, modify_prompt, modify_chat_history
+from utils.components import show_response_count, finish_button, show_finish_status
+from utils.chatbot import get_response
+from utils.database import submit_to_database
+
+st.set_page_config(
+    layout='wide',
+    page_title='Chatbot for science and technology',
+    page_icon='🤖'
+)
 
 # Streamed response emulator
 def response_generator():
@@ -16,28 +26,37 @@ def response_generator():
         yield word + " "
         time.sleep(0.05)
 
+def main():
 
-st.title("Simple chat")
+    # Set up session
+    session_setup()
 
-# Initialize chat history
-if "messages" not in st.session_state:
-    st.session_state.messages = []
+    # Show information
+    st.title('Welcome to the chat!')
+    st.info('Your goal is to find out the information with GPT-4 about **BRAIN CHIPS**. Please stay on topic!')
+    
+    # Initialize chat history
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
 
-# Display chat messages from history on app rerun
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+    # Display chat messages from history on app rerun
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
 
-# Accept user input
-if prompt := st.chat_input("What is up?"):
-    # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    # Display user message in chat message container
-    with st.chat_message("user"):
-        st.markdown(prompt)
+    # Accept user input
+    if prompt := st.chat_input("What is up?"):
+        # Add user message to chat history
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        # Display user message in chat message container
+        with st.chat_message("user"):
+            st.markdown(prompt)
 
-    # Display assistant response in chat message container
-    with st.chat_message("assistant"):
-        response = st.write_stream(response_generator())
-    # Add assistant response to chat history
-    st.session_state.messages.append({"role": "assistant", "content": response})
+        # Display assistant response in chat message container
+        with st.chat_message("assistant"):
+            response = st.write_stream(response_generator())
+        # Add assistant response to chat history
+        st.session_state.messages.append({"role": "assistant", "content": response})
+
+if __name__ == '__main__':
+    main()
