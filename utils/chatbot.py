eab0a193a91b683c713b3d30b271c125addf729a
Anqi Shao <anqi.shao@wisc.edu>
2024-05-05 11:21:00 -0500

    Update chatbot.py

--- utils/chatbot.py
+++ utils/chatbot.py
@@ -56,7 +56,7 @@ def request_response(user_input):
     prompt = prompt + 'Human: ' + user_input + '\n' + 'AI:'
     
     response = openai.ChatCompletion.create(
-        model="gpt-4-turbo-2024-04-09",
+        model="gpt-4-turbo",
         messages=[{"role": "user", "content": prompt}],
         temperature = 0
         #max_tokens=300
@@ -78,8 +78,8 @@ def request_response(user_input):
 
     content = response["choices"][0]["message"]["content"]
 
-    if content_filter(content) != '2':
-        return content
+    #if content_filter(content) != '2':
+        #return content
     return request_response(user_input)
 
 
