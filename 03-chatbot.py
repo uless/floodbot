70cb42879da0d759570622a1b58ba7a29491c67e
Anqi Shao <anqi.shao@wisc.edu>
2024-11-11 16:18:59 -0600

    Update 03-chatbot.py

--- 03-chatbot.py
+++ 03-chatbot.py
@@ -144,7 +144,7 @@ def main():
         st.session_state.messages = []
 
     if 'avatars' not in st.session_state:
-        st.session_state['avatars'] = 'chatbot_avatar.png'  # Set initial avatar
+        st.session_state['avatars'] = 'chatbot_avatar.webp'  # Set initial avatar
 
     # Display chat messages from history on app rerun
     for message in st.session_state.messages:
