ab23f04cd7124435a81e724b7b8e81ad7bfc1356
Anqi Shao <anqi.shao@wisc.edu>
2025-05-14 15:59:42 -0500

    Update 03-chatbot-both.py

--- 03-chatbot-both.py
+++ 03-chatbot-both.py
@@ -162,7 +162,6 @@ def main():
         border: 1px solid #ccc;">
         If you have any questions, please do not hesitate to chat with Jamie, our <b>interactive chatbot</b>!<br><br>
         Press 'Start chat' to begin. <br>
-        To make sure we follow a fair and transparent process, we use location data to match people with the nearest available aid. Once you start, Can you tell me your <b>ZIP code</b> so I can check what’s accessible for you?
 
     </div>
     """,
